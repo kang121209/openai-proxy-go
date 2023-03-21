0f281c7665d9bf90692622845ae0ca42f6351634
kang121209 <127708228+kang121209@users.noreply.github.com>
2023-03-21 22:03:31 +0800

    Update main.go

--- main.go
+++ main.go
@@ -48,7 +48,7 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 	// 将原始请求头复制到新请求中
 	for headerKey, headerValues := range r.Header {
 		for _, headerValue := range headerValues {
-			if strings.ToLower(headerKey) != strings.ToLower('host') {
+			if strings.ToLower(headerKey) != strings.ToLower("host"){
 			    proxyReq.Header.Add(headerKey, headerValue)
 			}
 		}
